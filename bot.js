00960f8e8ebe5b9212c8bb0c711be8ff91b8a1b3
bestdeveloperforever <42047034+bestdeveloperforever@users.noreply.github.com>
2018-11-13 00:27:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,4 +18,4 @@ client.on('ready', () => {
 });
 
 
-client.login("NTA4MjYzMjEyNjcyNjE0NDAx.DsGTWg.T3FipxFPFZRFPCW_VuvZS7j8ZVg");
+cclient.login("NTA4MjYzMjEyNjcyNjE0NDAx.DsGTWg.T3FipxFPFZRFPCW_VuvZS7j8ZVg");
